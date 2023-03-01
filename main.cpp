7ed5e3edeafb194534227df78d2b2358aaae680d
Stepan Karpov <stepan.karpov@list.ru>
2023-03-01 09:57:25 +0300

    Bridge added

--- main.cpp
+++ main.cpp
@@ -2,6 +2,49 @@
 #include <iostream>
 #include <vector>
 
+class Shape;
+class Color;
+
+// Colors
+
+class Color {
+ public:
+  virtual int getColotID() const = 0;
+  virtual void printColorName() const = 0;
+};
+
+class NoColor : public Color {
+ public:
+  int getColotID() const final {
+    return 0;
+  }
+  void printColorName() const final {
+    std::cout << "I have no color!\n";
+  }
+};
+
+class Red : public Color {
+ public:
+  int getColotID() const final {
+    return 1;
+  }
+  void printColorName() const final {
+    std::cout << "I am red!\n";
+  }
+};
+
+class Blue : public Color {
+ public:
+  int getColotID() const final {
+    return 2;
+  }
+  void printColorName() const final {
+    std::cout << "I am blue!\n";
+  }
+};
+
+// Shapes
+
 class Shape {
  public:
   const double PI = 3.1415;
@@ -11,6 +54,7 @@ class Shape {
 class Square : public Shape {
  public:
   double a;
+  Red my_color;
 
  Square(double a) : a(a) {}
 
@@ -22,12 +66,14 @@ class Square : public Shape {
 class Circle : public Shape {
  public:
   double r;
+  Blue my_color;
 
  Circle(double r) : r(r) {}
 
  double perimeter() const final {
   return 2 * Shape::PI * r;
  }
+ 
 };
 
 int main() {
@@ -41,12 +87,22 @@ int main() {
   // but they were created in a two different ways
   std::cout << square.perimeter() << "\n";
   std::cout << circle.perimeter() << "\n";
-
-
+  std::cout << "\n";
 
   // an example of bridge pattern
 
-   
+  Red red;
+  Blue blue;
+  NoColor blank;
+
+  red.printColorName();
+  blue.printColorName();
+  blank.printColorName();
+
+  std::cout << "\ncalling functions from shapes:\n";
+
+  square.my_color.printColorName();
+  circle.my_color.printColorName();
 
   return 0;
 }
